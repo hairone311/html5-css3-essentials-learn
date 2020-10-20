eacba718b18a126e51a442fa950c5156cce6f009
hairone311 <eliseuarviscoh@gmail.com>
2020-10-20 13:02:03 +0200

    Adicionando eventos ao menu

---
+++ 07-responsivando-layout/_cdn/main.js
@@ -0,0 +1,8 @@
+$(
+    function () {
+        $('.main_header_content_menu_mobile_obj').on('click', function () {
+            $('.main_header_content_menu_mobile_sub').toggleClass('ds_none');
+            $(this).toggleClass('main_header_content_menu_mobile_obj_active');
+        })
+    }
+);
